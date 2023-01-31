fbf907378969e1c80b4b3df241be305117ef5211
Todor <todormarinov02@gmail.com>
2023-01-31 23:34:44 +0200

    seed

--- BooksProject/Seeder/BookSeeder.cs
+++ BooksProject/Seeder/BookSeeder.cs
@@ -1,10 +1,64 @@
-﻿using CsvHelper;
-
+﻿using BooksProject.Context;
+using BooksProject.Models;
+using CsvHelper;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace BooksProject.Seeder
 {
     public class BookSeeder : IBookSeeder
     {
         private const string CsvPath = "C:\\Users\\GRIGS\\Desktop\\API4\\bestsellers with categories.csv";
+        private static IEnumerable<T> GetItemsFromCsv<T>(string directory)
+        {
+            IEnumerable<T> items;
+
+            using (var reader = new StreamReader(directory))
+            {
+                using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                items = csv.GetRecords<T>().ToArray();
+            }
+
+            return items;
+        }
+
+        private readonly AppDbContext dbContext;
+
+        public BookSeeder(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task SeedAsync()
+        {
+            bool isSeedRequred = !await this.dbContext.Books.AnyAsync();
+
+            if (!isSeedRequred)
+            {
+                return;
+            }
+
+            IEnumerable<BookCSVModel> BookCsvModels = GetItemsFromCsv<BookCSVModel>(CsvPath);
+
+            var BookDataModels = BookCsvModels
+                .Select(x => new Book
+                {
+                    Name = x.Name,
+                    Author = x.Author,
+                    Year = x.Year,
+                    Genre = x.Genre,
+                    Price = x.Price,
+                    Rating = x.Rating,
+                    Reviews = x.Reviews,
+                })
+                .ToList();
+
+            this.dbContext.AddRange(BookDataModels);
+
+            this.dbContext.SaveChanges();
+        }
     }
 }
+    
+
